29bb7f9ecd135d8b8afdef3a6672482c8f3c5eac
memesmith0 <meme.smith.wizard@gmail.com>
2025-04-11 07:08:42 -0700

    Update calculator.c
    sdofijsdofijodsifj

--- calculator.c
+++ calculator.c
@@ -684,20 +684,21 @@ int main(){while(a){b=getchar();switch(b){case 'g':p[d].i=0;t=p;a=0;break;defaul
     case '0':r[(t+1)->i].i=0;break;//zero
     case 'x':c[h++].p=t;t=r[(t+1)->i].p;break;//execute
     case '#':r[(t+1)->i].i=((r[(t+1)->i].i)*10)+(((t+2)->i)-48);break;//digit
-    case '+':r[(t+1)->i].i=r[(t+2)->i].i+r[(t+3)->i].i;r[(t+1)->i].p=r[(t+2)->i].p+r[(t+3)->i].i;break;//add
-    case '-':r[(t+1)->i].i=r[(t+2)->i].i-r[(t+3)->i].i;r[(t+1)->i].p=r[(t+2)->i].p-r[(t+3)->i].i;break;//subtract
-    case '*':r[(t+1)->i].i=r[(t+2)->i].i*r[(t+3)->i].i;break;//multiply
-    case '&':r[(t+1)->i].i=r[(t+2)->i].i&&r[(t+3)->i].i;break;//and
-    case '|':r[(t+1)->i].i=r[(t+2)->i].i||r[(t+3)->i].i;break;//or
-    case '/':r[(t+1)->i].i=r[(t+2)->i].i/r[(t+3)->i].i;break;//divide
-    case '!':r[(t+1)->i].i=!r[(t+2)->i].i;break;//not
-    case '%':r[(t+1)->i].i=r[(t+2)->i].i%r[(t+3)->i].i;break;//mod
-    case '=':r[(t+1)->i].i=r[(t+2)->i].i==r[(t+3)->i].i;break;//equal?
-    case 'm':r[(t+1)->i].p=malloc(sizeof(m)*r[(t+2)->i].i);break;//malloc
+    case '+':r[(t+1)->i].i+=r[(t+2)->i].i;break;//add
+    case 'j':r[(t+1)->i].p+=r[(t+2)->i].p;break;//jump
+    case '-':r[(t+1)->i].i*=-1;break;//negate
+    case '*':r[(t+1)->i].i*=r[(t+2)->i].i;break;//multiply
+    case '|':r[(t+1)->i].i=r[(t+1)->i].i||r[(t+2)->i].i;break;//or
+    case '/':r[(t+1)->i].i/=r[(t+2)->i].i;break;//divide
+    case '!':r[(t+1)->i].i=!r[(t+1)->i].i;break;//not
+    case '%':r[(t+1)->i].i=r[(t+1)->i].i%r[(t+2)->i].i;break;//mod
+    case '=':r[(t+1)->i].i=r[(t+1)->i].i==r[(t+2)->i].i;break;//equal?
+    case 'm':r[(t+1)->i].p=malloc(sizeof(m)*r[(t+1)->i].i);break;//malloc
     case 'f':free(r[(t+1)->i].p);break;//free
     case 'r':r[(t+1)->i].i=(*(r[(t+2)->i].p)).i;r[(t+1)->i].p=(*(r[(t+2)->i].p)).p;break;//read
     case 'w':r[(t+1)->i].p->i=r[(t+2)->i].i;r[(t+1)->i].p->p=r[(t+2)->i].p;break;//write
     case 'i':r[(t+1)->i].i=getchar();break;//input
     case 'o':putchar(r[(t+1)->i].i);break;//output
     case 'q':return 0;break;//quit
-    default :return 0;break;}t+=4}}//error
+    default :return 0;break;}t+=3}}//error
+
